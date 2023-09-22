dfa46518c2504b61038b1dd1264019c9ed1fd3ab
Manesh <manisht@fin-techsolutions.com>
2023-09-22 16:41:44 +0530

    test 4

--- src/main/java/com/example/MyWebAppApplication.java
+++ src/main/java/com/example/MyWebAppApplication.java
@@ -8,7 +8,6 @@ public class MyWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebAppApplication.class, args);
-        System.out.println("Test 1");
     }
 
 }
