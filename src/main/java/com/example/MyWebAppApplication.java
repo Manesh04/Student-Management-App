6a2651ab47c32a140961a0159b095601d69d15c8
Manesh <manisht@fin-techsolutions.com>
2023-09-25 14:41:41 +0530

    test1 25 Sept 23

--- src/main/java/com/example/MyWebAppApplication.java
+++ src/main/java/com/example/MyWebAppApplication.java
@@ -8,6 +8,7 @@ public class MyWebAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyWebAppApplication.class, args);
+        System.out.println("test 25-09-2023");
     }
 
 }
